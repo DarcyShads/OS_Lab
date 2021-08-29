6afd11a5ebae8e1243b9505be836d5ffba90aa24
DarcyShads <shadowdark630@gmail.com>
2021-08-29 12:19:23 +0530

    lab 3 added

---
+++ lab3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/wait.h>
+
+int main()
+{
+    int pid=fork();
+    if(pid==0)
+    {
+        sleep(5); //sleeps for 5 sec
+        printf("Child process id : %d \tParent process id : %d\n",getpid(),getppid());
+    }
+    else if(pid>0)
+    {
+        wait(NULL); //waits for the child process to execute
+        printf("Parent process id : %d \tGrand Parent process id : %d\n",getpid(),getppid());
+    }
+    else
+    {
+        printf("Child process could not be created\n");
+    }
+    return 0;
+}
